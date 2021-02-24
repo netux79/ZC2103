a4958d0953a0b5712fb0408b11871339f487eefb
netux79 <netux79@gmail.com>
2021-02-24 15:20:11 -0600

    First status of the project.

---
+++ ZELDADAT.H
@@ -0,0 +1,34 @@
+/* Allegro datafile object indexes, produced by grabber v4.1.12 (WIP), MinGW32 */
+/* Datafile: f:\zc\zelda.dat */
+/* Date: Sun Feb 01 14:50:27 2004 */
+/* Do not hand edit! */
+
+#define _SIGNATURE                       0        /* DATA */
+#define BMP_MOUSE                        1        /* BMP  */
+#define BMP_TITLE_NES                    2        /* BMP  */
+#define MUSIC                            3        /* FILE */
+
+#define MUSIC_DUNGEON                    0        /* MIDI */
+#define MUSIC_ENDING                     1        /* MIDI */
+#define MUSIC_GAMEOVER                   2        /* MIDI */
+#define MUSIC_LEVEL9                     3        /* MIDI */
+#define MUSIC_OVERWORLD                  4        /* MIDI */
+#define MUSIC_TITLE                      5        /* MIDI */
+#define MUSIC_TRIFORCE                   6        /* MIDI */
+#define MUSIC_COUNT                      7
+
+#define PAL_CREDITS                      4        /* PAL  */
+#define PAL_GUI                          5        /* PAL  */
+#define RLE_AGTEXT                       6        /* RLE  */
+#define RLE_CREDITS                      7        /* RLE  */
+#define TITLE_DX                         8        /* FILE */
+
+#define TITLE_DX_BMP_1                   0        /* BMP  */
+#define TITLE_DX_BMP_2                   1        /* BMP  */
+#define TITLE_DX_BMP_3                   2        /* BMP  */
+#define TITLE_DX_BMP_4                   3        /* BMP  */
+#define TITLE_DX_PAL_1                   4        /* PAL  */
+#define TITLE_DX_COUNT                   5
+
+#define WAV_00_AGFIRE                    9        /* SAMP */
+
